029e67582c88bc8d7add651a31d36cf13ade89f8
Drew Pirrone-Brusse <dpirrone-brusse@basho.com>
2015-04-26 14:46:16 -0400

    Add script to quickly set up remotes.

---
+++ subtree_remotes.sh
@@ -0,0 +1,25 @@
+git remote | grep riak_docs
+if [ $? -ne 0 ]; then # there is not a riak_doc remote...
+    echo "Adding http://github.com/TheOnlyRew/riak_docs as remote 'riak_docs...'"
+    git remote add riak_docs http://github.com/TheOnlyRew/riak_docs --fetch
+fi
+
+git remote | grep riakcs_docs
+if [ $? -ne 0 ]; then # there is not a riak_doc remote...
+    echo "Adding http://github.com/TheOnlyRew/riakcs_docs as remote 'riakcs_docs...'"
+    git remote add riakcs_docs http://github.com/TheOnlyRew/riakcs_docs --fetch
+fi
+
+echo
+echo "Your remotes are correctly set up!"
+echo "Below you'll find some useful commands."
+echo ""
+echo "To add subtrees (e.g. to create a new deploy/ branch from master),"
+echo "  * git subtree add --squash -P source/languages/en/riak riak_docs <branch>"
+echo "  * git subtree add --squash -P source/languages/en/riakcs riakcs_docs <branch>"
+echo "Optionally add a '-m <commit message>' to make the resulting merge messages"
+echo "more descriptive."
+echo ""
+echo "To pull in recent changes into existing subtrees,"
+echo "  * git subtree pull --squash -P source/languages/en/riak riak_docs <branch>"
+echo "  * git subtree pull --squash -P source/languages/en/riakcs riakcs_docs <branch>"
